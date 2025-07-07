be2c470100ec8c31d158c1b5ef48ad1068e1c7df
Manabu Inoue <inoue.manabu@tis.co.jp>
2025-07-07 12:10:36 +0000

    Initial

---
+++ srv/src/main/java/customer/cap01/SecurityConfig.java
@@ -0,0 +1,31 @@
+package customer.cap01;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+// @Profile("cloud")
+@Order(1)
+public class SecurityConfig {
+    @Autowired
+    private JwtDecoder jwtDecoder;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher("/**")
+            .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/public/**").permitAll()
+            .anyRequest().authenticated())
+            .oauth2ResourceServer(oauth2 -> oauth2
+            .jwt(jwt -> jwt
+            .decoder(jwtDecoder)));
+        return http.build();
+    }
+}
