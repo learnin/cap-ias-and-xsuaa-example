05f6cb98f41be698ec24dd74df1115dfa8e35c7c
learnin <i.learnin@gmail.com>
2025-09-13 15:41:47 +0900

    CSRF トークンチェックは不要なため無効化

--- srv/src/main/java/customer/cap01/SecurityConfig.java
+++ srv/src/main/java/customer/cap01/SecurityConfig.java
@@ -21,8 +21,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.securityMatcher("/**")
+            // CAP アプリでは Authorization ヘッダの JWT で認証するため CSRF トークンチェックは不要。ユーザからのアクセスは App Router で CSRF トークンチェックが行われる。
+            .csrf(c -> c.disable())
             .authorizeHttpRequests(authorize -> authorize
-            // .requestMatchers("/public/**").permitAll()
             .anyRequest().authenticated())
             .oauth2ResourceServer(oauth2 -> oauth2
             .jwt(jwt -> jwt
