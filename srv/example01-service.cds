d218b39d349af3e2328cbc48a62df11127accbce
learnin <i.learnin@gmail.com>
2025-09-15 18:56:13 +0900

    ダウンロード処理を実装

--- srv/example01-service.cds
+++ srv/example01-service.cds
@@ -4,13 +4,39 @@ using {
  } from '../db/data-model';
 
 service Example01Service {
+    @Core.MediaType: 'text/csv' @Core.ContentDisposition.Filename: 'hoge.csv' @Core.ContentDisposition.Type: 'attachment'
+    type CsvFile : LargeBinary;
+
     @restrict: [{grant: ['READ','WRITE'], to: ['Admin', 'internal-user']},
                 {grant: ['READ'], to: 'Viewer'}]
     entity Items as projection on _Items;
 
     @restrict: [{grant: ['READ','WRITE'], to: ['Admin', 'internal-user']},
                 {grant: ['READ'], to: 'Viewer'}]
-    entity Books as projection on _Books;
+    entity Books as projection on _Books actions {
+        // これだと /odata/v4/Example01Service/Books/Example01Service.download() へのアクセスで
+        // org.apache.olingo.server.core.ODataHandlerException: not implemented が発生する。
+        // https://cap.cloud.sap/docs/cds/cdl#actions-returning-media に記載されているやり方だが CAP Java というか Olingo では未対応か。
+        function download(in: many $self) returns CsvFile;
 
-    action upload(file: LargeBinary @Core.MediaType: 'text/csv') returns String;
+        // これだと /odata/v4/Example01Service/Books/Example01Service.download2() へのアクセスで
+        // com.sap.cds.reflect.CdsElementNotFoundException: No element with name 'download' in 'Example01Service.Books' が発生する
+        // function download2(in: many $self) returns LargeBinary @Core.MediaType: 'text/csv' @Core.ContentDisposition.Filename: 'hoge.csv' @Core.ContentDisposition.Type: 'attachment';
+    };
+
+    @restrict: [{grant: 'READ', to: ['Admin', 'Viewer', 'internal-user']}]
+    entity CsvFileEntity {
+        key id : Integer;
+
+        // これは /odata/v4/Example01Service/CsvFileEntity(1)/data へのアクセスでファイルがダウンロードされる。
+        // なお、 /odata/v4/Example01Service/CsvFileEntity/data へのアクセスだと The property 'data' must not follow a collection. で400エラーになる。
+        virtual data : LargeBinary @Core.MediaType: 'text/csv' @Core.ContentDisposition.Filename: 'hoge.csv' @Core.ContentDisposition.Type: 'attachment';
+    }
+
+    // これだと /odata/v4/Example01Service/download() へのアクセスで org.apache.olingo.server.core.ODataHandlerException: not implemented が発生する
+    function download() returns CsvFile;
+
+    // これだと /odata/v4/Example01Service/download() へのアクセスで
+    // java.lang.NullPointerException: Cannot invoke "org.apache.olingo.server.api.uri.UriResourcePartTyped.getType()" because the return value of "com.sap.cds.adapter.odata.v4.processors.request.CdsODataRequest.getLastEntityResource(boolean)" is null が発生する
+    // function download() returns　LargeBinary @Core.MediaType: 'text/csv' @Core.ContentDisposition.Filename: 'hoge.csv' @Core.ContentDisposition.Type: 'attachment';
 }
--- srv/src/main/java/customer/cap01/handler/Example01Handler.java
+++ srv/src/main/java/customer/cap01/handler/Example01Handler.java
@@ -1,21 +1,33 @@
 package customer.cap01.handler;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.sap.cds.services.cds.CdsReadEventContext;
 import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.After;
+import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
 import com.sap.cds.services.request.UserInfo;
 
+import cds.gen.example01service.Books;
+import cds.gen.example01service.BooksDownloadContext;
+import cds.gen.example01service.CsvFileEntity;
+import cds.gen.example01service.DownloadContext;
 import cds.gen.example01service.Example01Service_;
 import cds.gen.example01service.Items;
+
 
 @Component
 @ServiceName(Example01Service_.CDS_NAME)
@@ -45,5 +57,53 @@ public class Example01Handler implements EventHandler {
         items.add(item);
         logger.info(items.size() + " records");
         return items;
+    }
+
+    // /odata/v4/Example01Service/Books/Example01Service.download() に対するハンドラ。
+    // org.apache.olingo.server.core.ODataHandlerException: not implemented が発生する。
+    @On(event = BooksDownloadContext.CDS_NAME)
+    public void download(BooksDownloadContext context) throws IOException {
+        Path path = Files.createTempFile(null, null);
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            writer.append("1,'a'");
+            writer.newLine();
+            writer.append("2,'b'");
+            writer.newLine();
+        }
+        context.setResult(Files.newInputStream(path, StandardOpenOption.DELETE_ON_CLOSE));
+        context.setCompleted();
+    }
+
+    // /odata/v4/Example01Service/download() に対するハンドラ。
+    // org.apache.olingo.server.core.ODataHandlerException: not implemented が発生する。
+    @On(event = DownloadContext.CDS_NAME)
+    public void download(DownloadContext context) throws IOException {
+        Path path = Files.createTempFile(null, null);
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            writer.append("1,'a'");
+            writer.newLine();
+            writer.append("2,'b'");
+            writer.newLine();
+        }
+        context.setResult(Files.newInputStream(path, StandardOpenOption.DELETE_ON_CLOSE));
+        context.setCompleted();
+    }
+
+    // /odata/v4/Example01Service/CsvFileEntity(1)/data に対するハンドラ。
+    @On(event = CqnService.EVENT_READ)
+    public List<CsvFileEntity> download(List<CsvFileEntity> csvFileEntities) throws IOException {
+        Path path = Files.createTempFile(null, null);
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            writer.append("1,'a'");
+            writer.newLine();
+            writer.append("2,'b'");
+            writer.newLine();
+        }
+        CsvFileEntity csvFileEntity = CsvFileEntity.create();
+        csvFileEntity.setId(1);
+        csvFileEntity.setData(Files.newInputStream(path, StandardOpenOption.DELETE_ON_CLOSE));
+        csvFileEntities = new ArrayList<>();
+        csvFileEntities.add(csvFileEntity);
+        return csvFileEntities;
     }
 }
